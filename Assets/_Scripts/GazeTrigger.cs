979f06c0df42f9d39b9d485b4a82cd906e570d10
Chaojin <jinchaoonly@gmail.com>
2017-12-09 16:37:54 -0500

    Add gaze Trigger

---
+++ Assets/_Scripts/GazeTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeTrigger : MonoBehaviour {
+
+	public Camera PlayerCamera;
+	public AudioSource AS;
+
+	// Use this for initialization
+	void Start () {
+
+		AS = this.gameObject.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 camLookDir = PlayerCamera.transform.forward;
+
+		Vector3 VectorFromCamToTarget = transform.position - PlayerCamera.transform.position;
+
+		float angle = Vector3.Angle (camLookDir, VectorFromCamToTarget);
+
+		if (angle < 15f * transform.localScale.x) {
+
+			AS.Play ();
+		}
+		else {
+			AS.Pause();
+		}
+}
